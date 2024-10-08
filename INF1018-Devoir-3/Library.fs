3518852120615c7c57234a10ea970a5a783ac67a
William Flageol <wflageol@gmail.com>
2024-10-08 11:46:58 -0400

    More fixes

--- INF1018-Devoir-3/Library.fs
+++ INF1018-Devoir-3/Library.fs
@@ -21,28 +21,6 @@ module Interpreter =
     type Var = A | B | C
     type Op = Plus | Minus | Multiply | Divide
     type LexicalUnit = Var of Var | Int of int | Op of Op | Nil
-    
-    type Factor = VarFactor of Var
-                | IntFactor of int
-    type PartialExpression = PartialExpression of (Op * Factor * PartialExpression)
-                           | EmptyExpression
-    type Expression = ParenExpression of Expression
-                    | FactorExpression of Factor * PartialExpression
-
-    type State = { LexicalStack: Stack<LexicalUnit> }
-
-    let lexPeek state = 
-        if state.LexicalStack.Count > 0
-        then state.LexicalStack.Peek ()
-        else Nil
-
-    let lexPop state = state.LexicalStack.Pop ()
-    
-    let validateNext state lexUnit =
-        let next = lexPop state
-        if next <> lexUnit
-        then sprintf "Validation error: expected %A, got %A." lexUnit next
-             |> failwith
 
     // Reçoit une séquence d'unité lexicales et retourne le résultat de l'expression
     // si la séquence est valide pour la grammaire. Les valeurs des variables a, b et c
